0a953bb6fa894fd417033ff21e555bb2e84068dc
max fabin <maxikfabin@gmail.com>
2023-05-06 03:41:03 +0300

    .

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
+    origin: ['http://localhost:5173','https://lms-client.onrender.com']
 }))
 app.use('/', router)
 
