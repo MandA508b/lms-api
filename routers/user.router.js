497c5df60dab35f433f8daaf67d50671dd6353d1
Andriy Derkach <andriyderk@gmail.com>
2023-06-15 19:17:37 +0300

    !

--- routers/user.router.js
+++ routers/user.router.js
@@ -6,7 +6,7 @@ router.post('/login', userController.login)
 router.post('/registration', userController.registration)
 router.post('/refresh', userController.refresh)
 router.get('/findAll', userController.findAll)
-router.post('/findById', userController.findById)
+router.get('/findById', userController.findById)
 router.post('/logout', userController.logout)
 
 module.exports = router
