14a401c6b3c6c3ebbaae1695014d0381674bfe00
Andriy Derkach <andriyderk@gmail.com>
2023-08-22 16:01:06 +0300

    fix

--- middlewares/student.middleware.js
+++ middlewares/student.middleware.js
@@ -13,7 +13,7 @@ module.exports = (req, res, next) =>{
             return next(ApiError.unauthorized('Користувач не авторизаваний'))
         }
 
-        if(userData.role !== 'student' || userData.role !== 'admin'){
+        if(userData.role !== 'student' && userData.role !== 'admin'){
             return next(ApiError.forbidden('Користувач не є студентом'))
         }
 
